ad903dff2406901ad0bc43e487a341ed418c59d8
이정석 <jsuk10@naver.com>
2020-10-10 08:10:56 +0900

    1929 std::ios::sync_with_stdio(false);을 추가 했음에도 endl이 아주 느리기 떄문에 시간 초과가 됨. endl - \n으로 교체하니 정답.

--- 1929.cpp
+++ 1929.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
 #define MaxNum 1000001
 bool *primeArray = new bool[MaxNum];
 
-void Eratos()
+void Eratos(int end)
 {
     //초기화
     //전역 변수 선언시 false값으로 초기화 되어 있음.
-    for (int i = 2; i <= MaxNum; i++)
+    for (int i = 2; i <= end; i++)
         primeArray[i] = true;
 
-    for (int i = 2; i * i <= MaxNum; i++)
+    for (int i = 2; i * i <= end; i++)
     {
         if (primeArray[i] == true)
-            for (int j = i * i; j <= MaxNum; j += i)
+            for (int j = i * i; j <= end; j += i)
                 primeArray[j] = false;
     }
 }
@@ -25,16 +24,16 @@ void InputNum(int start, int last)
     for (int i = start; i <= last; i++)
     {
         if (primeArray[i] == true)
-            cout << i << endl;
+            cout << i << "\n";
     }
 }
 
 int main()
 {
     std::ios::sync_with_stdio(false);
-    Eratos();
-    
+
     int start, end;
     cin >> start >> end;
+    Eratos(end);
     InputNum(start, end);
 }
