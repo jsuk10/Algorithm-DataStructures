3e8029c47690aac7a5bb6e847eef21324123b2e4
jsuk10 <jsuk10@naver.com>
2021-01-02 19:35:29 +0900

    feat : Data Structures HanoiTower
    자료구조의 하노이탑 구현

---
+++ DataStructures/HanoiTower.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Process(int num, char from, char to, char by) {
+    if (num == 0)
+        return;
+    Process(num - 1, from , by, to);
+    cout << from << "에서" << to << "로" << num << "원반 이동 \n";
+    Process(num - 1, by, to, from);
+}
+
+int main() {
+    int num;
+    char A = 'S', B = 'D', C = 'T';
+    cin >> num;
+    Process(num, A,B,C);
+}
