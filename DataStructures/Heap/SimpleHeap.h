86a170385d7cd411718b37da64103bbb3ba8f585
jsuk10 <jsuk10@naver.com>
2021-01-24 17:00:33 +0900

    feat : SimpleHeap
    간단한 힙을 구현함
    이 힙은 우선순위를 내부에서 구현해야 했고, pr을 직접 설정해주는 문제가 있음.

---
+++ DataStructures/Heap/SimpleHeap.h
@@ -0,0 +1,35 @@
+#ifndef __SIMPLE_HEAP_H__
+#define __SIMPLE_HEAP_H__
+
+#define TRUE 1
+#define FALSE 0
+
+#define HEAP_LEN 100
+
+typedef char HData;
+typedef int Priority;
+
+typedef struct _heapElem
+{
+	Priority pr; // 값이 작을수록 높은 우선순위
+	HData data;
+} HeapElem;
+
+typedef struct _heap
+{
+	int numOfData;
+	HeapElem heapArr[HEAP_LEN];
+} Heap;
+
+/*** Heap 관련 연산들 ****/
+void HeapInit(Heap *ph);
+int HIsEmpty(Heap *ph);
+
+void HInsert(Heap *ph, HData data, Priority pr);
+HData HDelete(Heap *ph);
+
+int GetPartenIndex(int index);
+int GetRightChildIndex(int index);
+int GetLeftChildIndex(int index);
+int GetHipPriChildIndex(Heap *ph, int index);
+#endif
