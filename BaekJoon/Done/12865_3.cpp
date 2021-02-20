f43cecdcdd4b6872ed86a91006a84f4b2497036a
Leejeongseok0407 <jsuk10@naver.com>
2021-02-20 16:27:07 +0900

    feat : 평범한 배낭 골5
    dp를 이용한 배낭문제
    메모제이션을 어떻게 할지가 핵심인듯 하다.
    엄청나게 풀었는데 안풀려서 결국 답지봄.
    
    처음에는 백트래킹 시도하였으나 안되었고 이후 코드를 수정하니 시간 초과가 뜸.
    메모제이션 기법을 이용하여 이것을 해결했는데 방법은 이와 같음
    1. 물건들을 무게당 가치순으로 정렬
    2. 초기값(0번 물건)을 세팅
    3.이후 이전 줄과 비교하여 물건을 넣을 경우/넣지 않을경우로 나누어서 높은값을 갱신해나감
    4.최종적으로 기록된 값이 가장 높은값임
    
    많은 영상과 자료가 있으므로 이해 안되면 참고하기

--- BaekJoon/Done/12865_3.cpp
+++ BaekJoon/Done/12865_3.cpp
@@ -7,81 +7,47 @@ using namespace std;
 int arr[102][100002];
 int maxWeight, caseNum;
 
-//우선순위를 비교하기 위한 구조체
 class Stuff
 {
 public:
     int v;
     int w;
-    float wpv;
 
     Stuff(int w, int v)
     {
         this->v = v;
         this->w = w;
-        this->wpv = (float)w / (float)v;
     }
 };
 
-//우선순위 비교하기 위한 함수
-bool Compare(Stuff a, Stuff b)
-{
-    return a.wpv > b.wpv;
-}
-
-//구조체 담을 백터
 vector<Stuff> v;
 
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    //구조체 만들기 위해 잠시 받는 변수
     int tempWeight, tempValue;
-
-    //물건수랑, 최대 중량 담음
     cin >> caseNum >> maxWeight;
-
-    //물건 받음
     for (int i = 0; i < caseNum; i++)
     {
         cin >> tempWeight >> tempValue;
         v.push_back(Stuff(tempWeight, tempValue));
     }
 
-    //정렬
-    sort(v.begin(), v.end(), Compare);
-
-    //초기값 세팅
     for (int j = v[0].w; j <= maxWeight; j++)
     {
         arr[0][j] = v[0].v;
     }
-
-    //dp
+    
     for (int i = 1; i < caseNum; i++)
     {
         for (int j = 0; j <= maxWeight; j++)
         {
-            //낮으면 이전거 가지고옴
             if (j - v[i].w < 0)
             {
                 arr[i][j] = arr[i - 1][j];
                 continue;
             }
-            //높으면 배낭에 안 넣을경우 / 넣을 경우중 높은값 가지고옴
             arr[i][j] = max(arr[i - 1][j], v[i].v + arr[i - 1][j - v[i].w]);
         }
     }
-    //출력
-    
-    for (int i = 0; i < caseNum; i++)
-    {
-        for (int j = 0; j <= maxWeight; j++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << "\n";
-    }
-
     cout << arr[caseNum - 1][maxWeight];
 }
