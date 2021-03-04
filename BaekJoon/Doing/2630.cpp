baf7b53920c8f06c331c4fe86224331c8bbadd9a
jsuk10 <jsuk10@naver.com>
2021-03-04 19:27:34 +0900

    feat : 2630 doing

---
+++ BaekJoon/Doing/2630.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int arr[129][129], n;
+int answer[2];
+
+void quadTree(int X, int Y, int size)
+{
+    if (size == 0)
+        return;
+    int firstData = arr[X][Y];
+    bool isCombine = true;
+
+    for (int i = Y; i < Y + size; i++)
+    {
+        for (int j = X; j < X + size; j++)
+        {
+            if (firstData != arr[i][j])
+            {
+                isCombine = false;
+                break;
+            }
+        }
+        if (isCombine == false)
+            break;
+    }
+
+    //병합 가능 ^^
+    if (isCombine)
+    {
+        answer[firstData]++;
+        cout << X << " " << Y << " " << size << "\n";
+        return;
+    }
+
+    //응 앙대 분활해~
+    int halfSize = size / 2;
+    quadTree(X, Y, halfSize);
+    quadTree(X + halfSize, Y, halfSize);
+    quadTree(X, Y + halfSize, halfSize);
+    quadTree(X + halfSize, Y + halfSize, halfSize);
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    quadTree(0, 0, n);
+    cout << answer[0] << " " << answer[1];
+}
