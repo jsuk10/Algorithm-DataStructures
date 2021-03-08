3bccc1544afcc93cd5dc618b6bc9be5583b2630d
jsuk10 <jsuk10@naver.com>
2021-03-09 01:14:45 +0900

    feat : n과 m 7~9 실3, 실3, 실2
    n과 m (9)번이 좀 어려웠다.
    set을 이용해서 중복 제거는 금방 생각이 났는데
     string로 하다가 int를 char로 변환할때, string로 변환시 비교함수 같은 여러 난관에 봉착하여
     string에서 vector<int>로 선회했다.

---
+++ BaekJoon/Done/15663.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+
+using namespace std;
+
+vector<int> v, answer;
+set<vector<int>> av;
+set<string> sets;
+int arr[8];
+int n, m;
+
+void Process()
+{
+    //size = m되면 셋에 넣음
+    if (answer.size() == m)
+    {
+        av.insert(answer);
+        return;
+    }
+
+    //중복을 제거하는 백 트래킹
+    //arr[i]로 중복 체크함
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+            continue;
+        arr[i]++;
+        answer.push_back(v[i]);
+        Process();
+        arr[i]--;
+        answer.pop_back();
+    }
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    cin >> n >> m;
+
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        v.push_back(temp);
+    }
+
+    //set써서 사실 안해줘도됨
+    sort(v.begin(), v.end());
+    //과정
+    Process();
+
+    for (auto iter = av.begin(); iter != av.end(); iter++)
+    {
+        for (auto j = iter->begin(); j < iter->end(); j++)
+        {
+            cout << *j << " ";
+        }
+        cout << "\n";
+    }
+}
