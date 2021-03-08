f9a45621b391a14050f9421ca1c4121650dcf977
jsuk10 <jsuk10@naver.com>
2021-03-08 19:16:28 +0900

    feat : n과 m(5) 실3
    백트래킹 대표 문제 n과 m
    arr을 이용해서 체크 하는 방법 외에도 무엇이 있지 않을까 고민해보자

---
+++ BaekJoon/Done/15654.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v,answer;
+int arr[8];
+int n, m;
+
+void Process(int index)
+{
+    if (answer.size() == m)
+    {
+        for (int i = 0; i < answer.size(); i++)
+        {
+            cout << answer[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+            continue;
+        arr[i]++;
+        answer.push_back(v[i]);
+        Process(i);
+        arr[i]--;
+        answer.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(), v.end());
+    Process(0);
+}
