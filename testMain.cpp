2b7d034ecd15440d8f31136a8d268ba7b77405f6
jsuk10 <jsuk10@naver.com>
2025-08-14 14:29:24 +0900

    LeatCode - [Top Interview Questions] [Easy Collection]
    Array
    -- [Plus One] (https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/)

--- testMain.cpp
+++ testMain.cpp
@@ -8,34 +8,12 @@ using namespace std;
 
 int main()
 {
-	vector<int> nums = {99,-1,-100,3};
-	for (auto a : nums)
-	{
-		cout << a;
-	}
-	cout << endl;
-
+	vector<int> nums = {9};
 	Solution solution;
-	solution.rotate(nums, 2);
-
-	for (auto a : nums)
+	auto an = solution.plusOne(nums);
+	for (auto a : an)
 	{
-		cout << a;
+		cout << a ;
 	}
-	cout << endl;
-
-	vector<int> nums2 = {1,2,3,4,5,6,7};
-	for (auto a : nums2)
-	{
-		cout << a;
-	}
-	cout << endl;
-
-	solution.rotate(nums2, 3);
-
-	for (auto a : nums2)
-	{
-		cout << a;
-	}
-	cout << endl;
+	cout <<endl;
 }
