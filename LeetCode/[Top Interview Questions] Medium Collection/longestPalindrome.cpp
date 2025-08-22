a909f3f6cc727897654d2f37d9edd4b819789b8e
jsuk10 <jsuk10@naver.com>
2025-08-23 02:28:15 +0900

    leatCode [Top Interview Questions] Medium Collection
    Array
    
    -- [Longest Palindromic Substring] (https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/780/)

---
+++ LeetCode/[Top Interview Questions] Medium Collection/longestPalindrome.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+// Runtime  : 20ms     53.74%
+// Memory   : 35.3MB   29.24%
+
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        int length = 0;
+        string ans;
+
+        for (int centerIndex = 0; centerIndex < s.size(); ++centerIndex)
+        {
+            int L = centerIndex, R = centerIndex;
+            while (L - 1 >= 0 && s[centerIndex] == s[L - 1])
+            {
+                L--;
+            }
+
+            while (R + 1 <= s.length() && s[centerIndex] == s[R + 1])
+            {
+                R++;
+            }
+
+            while (L - 1 >= 0 && R + 1 <= s.length() && s[L - 1] == s[R + 1])
+            {
+                L--;
+                R++;
+            }
+
+            int len = R - L + 1;
+
+            ans = ans.length() > len ? ans : s.substr(L, R - L + 1);
+        }
+        return ans;
+    }
+};
